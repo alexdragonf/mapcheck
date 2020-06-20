f8f857a74546eec9c47206d520452a0b180c2a63
AlexDragon <fedal77@mail.ru>
2020-06-20 11:45:14 +0300

    initial commit

---
+++ apps/map-check/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@mapcheck/api-interfaces';
+
+@Component({
+  selector: 'mapcheck-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
