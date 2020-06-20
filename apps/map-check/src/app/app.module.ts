beeb341e4665fd4269e7f7ef1677582587503e69
AlexDragon <fedal77@mail.ru>
2020-06-20 21:05:17 +0300

    added dependencies

--- apps/map-check/src/app/app.module.ts
+++ apps/map-check/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
