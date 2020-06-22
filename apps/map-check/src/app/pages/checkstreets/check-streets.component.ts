3f4522ba01edb21e8b798c99b759f8da99f5b3b1
AlexDragon <fedal77@mail.ru>
2020-06-23 01:00:45 +0300

    some cosmetics fix

--- apps/map-check/src/app/pages/checkstreets/check-streets.component.ts
+++ apps/map-check/src/app/pages/checkstreets/check-streets.component.ts
@@ -1,3 +1,4 @@
+import { MapComponent } from './../map/map.component';
 import { IStreet, IHouse } from '@mapcheck/api-interfaces';
 import { HttpClient } from '@angular/common/http';
 import {
@@ -15,18 +16,21 @@ import {
   map,
   filter,
 } from 'rxjs/operators';
-import { Marker, Icon } from 'leaflet';
+import { Marker, Icon , LatLng, latLngBounds, Map, LatLngBounds, bounds} from 'leaflet';
 
 @Component({
   selector: 'mapcheck-street',
   templateUrl: './check-streets.component.html',
   styleUrls: ['./check-streets.component.scss'],
 })
-export class CheckStreetsComponent implements OnInit {
+export class CheckStreetsComponent implements OnInit, AfterViewInit {
   private streets: IStreet[];
   houses: IHouse[];
   public model: IStreet;
   public markers: Marker[] = [];
+  public centerMap: LatLng = new LatLng(47.994024, 37.801556);
+  public zoomMap = 12;
+  @ViewChild('map') map: MapComponent;
 
   constructor(private http: HttpClient) {}
 
@@ -50,26 +54,39 @@ export class CheckStreetsComponent implements OnInit {
         .get<IHouse[]>(`api/house?street-id=${event.id}`)
         .subscribe(this.setHouses)
     }
-  }
+  };
 
 setHouses = (houses) => {
+  // let _bounds: LatLngBounds; //TODO: Установить границы карты
   this.houses = houses;
+  if (houses) {
+    // _bounds = new LatLngBounds([[houses[0].lon, houses[0].lat], [houses[0].lon, houses[0].lat]])
+  // _bounds = new LatLngBounds([[47, 37],[47, 37]])
+  }
   this.markers = this.houses.map(
     (h) =>
-      new Marker([h.lon, h.lat], {
+      {
+        const _m= new Marker([h.lon, h.lat], {
         icon: new Icon({
           iconSize: [25, 41],
           iconAnchor: [13, 41],
           iconUrl: 'leaflet/marker-icon.png',
-          shadowUrl: 'leaflet/marker-shadow.png',
+          shadowUrl: 'leaflet/marker-shadow.png'
         }),
       })
-  );
+      // _bounds.extend([h.lon, h.lat])
+      _m.bindPopup(`${this.model.fullName}, д. ${h.house}`);
+      return _m
+    }
+    );
+    // this.map.setBounds(_bounds)
 };
 
-onHouseClick(no: string) {
-  console.log(no);
-  
+onHouseClick(i: number) {
+  // console.log(no);
+  this.centerMap = new LatLng(this.houses[i].lon, this.houses[i].lat);
+  this.zoomMap = 18;
+  this.markers[i].openPopup();
 }
 
 
@@ -78,4 +95,10 @@ onHouseClick(no: string) {
       .get<IStreet[]>('api/street')
       .subscribe((streets) => (this.streets = streets));
   }
+
+  ngAfterViewInit() {
+    console.log(this.map);
+    
+  }
+
 }
--- apps/map-check/src/app/pages/map/map.component.ts
+++ apps/map-check/src/app/pages/map/map.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
 import {
   TileLayer,
   tileLayer,
@@ -13,60 +19,56 @@ import {
   TileLayerOptions,
   Layer,
   MarkerClusterGroupOptions,
+  Map,
+  LatLngBounds
 } from 'leaflet';
-import {LeafletTileLayerDefinition} from '@asymmetrik/ngx-leaflet'
+import { LeafletTileLayerDefinition } from '@asymmetrik/ngx-leaflet';
 import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
-
 
 @Component({
   selector: 'mapcheck-map-window',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit, OnChanges {
   markerClusterOptions: MarkerClusterGroupOptions = {
-    disableClusteringAtZoom: 7
-  }
+    disableClusteringAtZoom: 7,
+  };
   mapOptions: MapOptions = {
     layers: [
       new TileLayer(
-        'http://vec0{s}.maps.yandex.net/tiles?l=map&z={z}&x={x}&y={y}&scale=2&lang=ru_RU', {
-         subdomains: '1234',
-         maxZoom: 21,
-         attribution: '...',
-         minZoom: 8,
-       })
-     ],
-     crs: CRS.EPSG3395
+        'http://vec0{s}.maps.yandex.net/tiles?l=map&z={z}&x={x}&y={y}&scale=2&lang=ru_RU',
+        {
+          subdomains: '1234',
+          maxZoom: 21,
+          attribution: '...',
+          minZoom: 8,
+        }
+      ),
+    ],
+    crs: CRS.EPSG3395,
   };
   // 37.801682	47.991914
   @Input() markers: Marker[] = [];
   @Input() centerMap: LatLng = new LatLng(47.994024, 37.801556);
   @Input() zoomMap = 12;
-  
-  constructor(private ref: ChangeDetectorRef) { }
+  map: Map;
+  constructor(private ref: ChangeDetectorRef) {}
 
   ngOnChanges(): void {}
 
   ngOnInit(): void {
     this.mapOptions.zoom = this.zoomMap;
     this.mapOptions.center = this.centerMap;
-
-
-    // this.mapOptions = {
-    //   layers: [
-    //    new TileLayer(this.yandexTileMapBaseURL, {
-    //       subdomains: '1234',
-    //       maxZoom: 21,
-    //       attribution: '...',
-    //       minZoom: 8,
-    //     })
-    //   ],
-    //   zoom: this.zoomMap,
-    //   center: this.centerMap,
-    //   crs: CRS.EPSG3395
-    // };
-
   }
 
+  onMapReady(map: Map) {
+    this.map = map;
+  }
+
+  setBounds(bounds: LatLngBounds) {
+    if (this.map) {
+      this.map.setMaxBounds(bounds);
+    }
+  }
 }
