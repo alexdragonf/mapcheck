59cd21b0d531b19be7600a3e12ae76c4cfcc8f88
Alex <fedal77@mail.ru>
2022-04-06 19:42:40 +0000

    merged

--- apps/map-check/src/app/services/district.service.ts
+++ apps/map-check/src/app/services/district.service.ts
@@ -3,13 +3,7 @@ import { Observable } from 'rxjs';
 import { DistrictInterface } from '@mapcheck/api-interfaces';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-<<<<<<< Updated upstream
 
-@Injectable({providedIn: 'root'})
-export class DistrictService {
-    apiURL = '/api/district';
-    constructor(private http: HttpClient) {}
-=======
 import { AppConfig } from '../app.config';
 
 // @Injectable({providedIn: 'root'})
@@ -22,7 +16,6 @@ export class DistrictService {
         private config: AppConfig) {
             // this.apiUrl = config.apiEndpoint.concat('/district')
         }
->>>>>>> Stashed changes
 
     getDistrictsById(id: number): Observable<DistrictInterface[]> {
         return this.http.get<DistrictInterface[]>(this.apiURL.concat(`?id=${id}`))
